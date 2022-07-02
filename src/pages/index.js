5ed7898a7154b8c257ad28ea339d94558053bf7c
Luis Martinez <luisgmp1997@gmail.com>
2022-07-01 20:33:46 -0400

    fix problems

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,70 +1,68 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+import Producto from "../components/Producto";
+import { getItems } from "../services/itemService";
+import style from "../styles/Home.module.css";
+import styleProduct from "../styles/Productos.module.css";
 
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import Producto from '../components/Producto'
-import { getLatestItems } from '../services/itemService'
-import style from '../styles/Home.module.css'
-import styleProduct from '../styles/Productos.module.css'
-
-export default function Home({items}) {
+export default function Home({ items }) {
   return (
     <Layout title="Bienvenido">
- <div className={style.banner}>
-          <div className={style.bannerBackground}>
-            <div className={style.bannerInfo}>
-              <h2>Todo lo que Deseas Esta  Aqui
-              </h2>
-              <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas placerat mauris at mauris
-               consequat, ut varius sem congue. Nullam blandit quam nec orci maximus.
-              </p>
-              <Link href='/tienda'>
-            <a className={style.link}> nuestra colecion</a>
-        </Link>
-            </div>
+      <div className={style.banner}>
+        <div className={style.bannerBackground}>
+          <div className={style.bannerInfo}>
+            <h2>Todo lo que Deseas Esta Aqui</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              placerat mauris at mauris consequat, ut varius sem congue. Nullam
+              blandit quam nec orci maximus.
+            </p>
+            <Link href="/tienda">
+              <a className={style.link}> nuestra colecion</a>
+            </Link>
           </div>
         </div>
+      </div>
 
       <h3>Latest Products</h3>
-        <div className={styleProduct.items}>
-          {items &&
-            items.map((item) => (
-              <Producto key={item.id} item={item} showAs={item} />
-            ))}
-        </div>
+      <div className={styleProduct.items}>
+        {items &&
+          items.map((item) => (
+            <Producto key={item.id} item={item} showAs={item} />
+          ))}
+      </div>
 
       <div className={style.galeria}>
-          <div className={style.item1}></div>
-          <div className={style.item2}></div>
-          <div className={style.item3}></div>
+        <div className={style.item1}></div>
+        <div className={style.item2}></div>
+        <div className={style.item3}></div>
       </div>
 
       <div className={style.articulo}>
-          <div className={style.bannerBackground}>
-            <div className={style.bannerInfo}>
-              <h2>Encuentra lo Que Deseas!
-              </h2>
-              <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas placerat mauris at mauris
-               consequat, ut varius sem congue. Nullam blandit quam nec orci maximus.
-              </p>
-              <Link href='/tienda'>
-            <a className={style.link}> nuestra colecion</a>
-        </Link>
-            </div>
+        <div className={style.bannerBackground}>
+          <div className={style.bannerInfo}>
+            <h2>Encuentra lo Que Deseas!</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              placerat mauris at mauris consequat, ut varius sem congue. Nullam
+              blandit quam nec orci maximus.
+            </p>
+            <Link href="/tienda">
+              <a className={style.link}> nuestra colecion</a>
+            </Link>
           </div>
         </div>
-
+      </div>
     </Layout>
-  )
+  );
 }
 
-export async function getStaticProps({items}){
-   const respuesta = await getLatestItems()
-
-   return{
-    props:{
-      items: respuesta,
+export async function getStaticProps() {
+  const res = await getItems();
+  console.log(typeof res);
+  return {
+    props: {
+      items: res.slice(0, 3),
     },
-   }
+  };
 }
--- src/services/itemService.js
+++ src/services/itemService.js
@@ -1,13 +1,5 @@
 export async function getItems() {
   const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/items`;
   const respuesta = await fetch(url);
-  const items = await respuesta.json();
-
-  return items;
-}
-
-export async function getLatestItems() {
-  const items = await getItems();
-
-  return items.slice(0, 3);
+  return respuesta.json();
 }
