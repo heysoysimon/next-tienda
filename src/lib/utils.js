6786e30ff88eb413bca56e5d6f37696422302f90
Luis Martinez <luisgmp1997@gmail.com>
2022-07-01 20:49:21 -0400

    add items

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,32 +1,31 @@
-import {getItems} from '../services/itemService'
+import { getItems } from "../services/itemService";
 
-export async function getIds(){
-    const items = await getItems()
+export async function getIds() {
+  const items = await getItems();
 
-    const ids = items.map(item =>{
-        return{
-            params:{
-                /* aqui van los ids de los objectos */
-                id: converPath(item.title),
-            }
-        }
-    })
+  const ids = items.map((item) => {
+    return {
+      params: {
+        /* aqui van los ids de los objectos */
+        id: converPath(item.title),
+      },
+    };
+  });
 
-    return ids
+  return ids;
 }
 
-export async function getItemData(id){
-    const items = await getItems()
+export async function getItemData(id) {
+  const items = await getItems();
 
-    const producto = items.find((item) => converPath(item.title) === id);
+  const producto = items.find((item) => converPath(item.title) === id);
 
-    return{
-        id: id,
-        data: producto
-    }
-
+  return {
+    id: id,
+    data: producto,
+  };
 }
 
-export function converPath(title){
-    return title.toLowerCase().replace(/\s/g, "-")
+export function converPath(title) {
+  return title.toLowerCase().replace(/\s/g, "-");
 }
